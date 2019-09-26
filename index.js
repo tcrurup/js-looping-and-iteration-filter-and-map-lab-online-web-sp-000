94aa9acd6940eaedda8a166d07e81a7cba8930e7
Anthony Rurup <tcrurup@gmail.com>
2019-09-26 20:04:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,16 +17,19 @@ function exactMatch(drivers, driverAttributes){
   
   return drivers.filter(function(driver){
     
-    
     let matching = true;
-    
     for(const key in driverAttributes){
       if(driverAttributes[key] !== driver[key]){
         matching = false;
       }
     }
-    
-    
     return matching;
+  });
+}
+
+function exactMatchToList(driver, driverAttributes){
+  
+  return exactMatch(driver, driverAttributes).map(function(driver){
+    return driver.name;
   });
 }
